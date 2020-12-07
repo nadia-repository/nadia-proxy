be5f7269aeb0d6c0a13da45fc5bb35ad79f7ac9b
shixiang <xiang.shi@oriente.com>
2020-12-07 23:43:40 +0800

    fix: arrary resize bug

--- src/util/arraylist.c
+++ src/util/arraylist.c
@@ -55,5 +55,6 @@ static void resize(ARRAYLIST *list){
         elementData[i] = list->elementData[i];
     }
     Free(list->elementData);
+    list-elementData = elementData;
     list->size = size;
 }
