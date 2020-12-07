4ee9ece7d469149412a141c5184fb9430faa4f89
xiangshi <xiang.shi@oriente.com>
2020-12-07 19:54:28 +0800

    feat: code arraylist

--- src/util/arraylist.c
+++ src/util/arraylist.c
@@ -1,3 +1,40 @@
 #include "arraylist.h"
 
+static void resize(ARRAYLIST *list);
 
+ARRAYLIST *init_arraylist(int size){
+    if(size == 0){
+        size = ARRAYLIST_DEFAULT_INITIAL_CAPACITY;
+    }
+    ARRAYLIST *list = malloc(sizeof(ARRAYLIST));
+    list->size = size;
+    list->elementData = calloc(list->size,sizeof(void *));
+    list->length = 0;
+    list->add = &add_arraylist;
+    list->get = &get_arraylist;
+    list->length = &arraylist_length;
+    return list;
+}
+
+void add_arraylist(ARRAYLIST *list, void *data){
+    if(list->length >=list->size){
+        resize(list);
+    }
+    list->elementData[list->length++] = data;
+}
+
+
+void *get_arraylist(ARRAYLIST *list){
+    if(list->length <= 0){
+        return NULL;
+    }
+    return list->elementData[list->length--];
+}
+
+int arraylist_length(ARRAYLIST *list){
+    return list->length;
+}
+
+static void resize(ARRAYLIST *list){
+
+}
--- src/util/arraylist.h
+++ src/util/arraylist.h
@@ -1,14 +1,34 @@
 #ifndef  ARRAYLIST_H 
 #define ARRAYLIST_H 
 
+#include <stdlib.h>
+
 #define ARRAYLIST_DEFAULT_INITIAL_CAPACITY 20
 #define ARRAYLIST_DEFAULT_LOAD_FACTOR 0.75
 
+#define ARRAYLIST_INIT init_arraylist(0)
+#define ARRAYLIST_ADD(list,data) list->add(list,data)
+#define ARRAYLIST_GET(list,data) list->get(list,data)
+#define ARRAYLIST_LENGTH(list) list->length(list)
 
 typedef struct arraylist_struct{
-    
+    void **elementData;
+    int size;
+    int length;
+
+    void (* add)(ARRAYLIST *list, void *data);
+    void *(* get)(ARRAYLIST *list);
+    int (* length)(ARRAYLIST *list);
 } ARRAYLIST;
 
+
+ARRAYLIST *init_arraylist(int size);
+
+void add_arraylist(ARRAYLIST *list, void *data);
+
+void *get_arraylist(ARRAYLIST *list);
+
+int arraylist_length(ARRAYLIST *list);
 
 
 #endif
